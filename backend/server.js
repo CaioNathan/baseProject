20c7ee249841c781926767910475b0d72af566a1
CaioNathanS <nathansallenave@gmail.com>
2021-04-29 16:36:32 -0300

    up2

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routers/userRouter.js';
 import atendRouter from './routers/atendRouter.js';
-
+import path from 'path';
 const app = express();
 
 app.use(express.json());
@@ -17,6 +17,17 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/newSystem
 
 app.use('/api/users', userRouter);
 app.use('/api/atend', atendRouter);
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// });
+
 
 
 app.get('/', (req, res) => {
